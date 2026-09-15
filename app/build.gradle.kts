43f833b2d04a215c475afb8f69095a668ac31f67
Freetime Maker <165287054+FreetimeMaker@users.noreply.github.com>
2026-09-15 20:45:32 +0200

    Remove undefined AndroidX runner dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,7 +57,6 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.runner)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
 
